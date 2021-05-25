d062055d7777902ca421eedb6b2ad24e067d834e
thestungyer <47674413+thestungyer@users.noreply.github.com>
2021-05-25 13:31:28 +0300

    made messages pages

--- app/src/main/java/com/example/buddyy/Message.java
+++ app/src/main/java/com/example/buddyy/Message.java
@@ -2,40 +2,50 @@ package com.example.buddyy;
 
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.shadow.ShadowDrawableWrapper;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Message extends AppCompatActivity {
+    MyRecyclerViewAdapter adapter;
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.messeger);
-        Objects.requireNonNull(getSupportActionBar()).hide();
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.home:
                         intent = new Intent(Message.this, Home.class);
                         startActivity(intent);
                         break;
                     case R.id.search:
-                        intent = new Intent(Message.this,Search.class);
+                        intent = new Intent(Message.this, Search.class);
                         startActivity(intent);
                         break;
                     case R.id.post:
-                        intent = new Intent(Message.this,Post.class);
+                        intent = new Intent(Message.this, Post.class);
                         startActivity(intent);
                         break;
 
@@ -43,16 +53,35 @@ public class Message extends AppCompatActivity {
 
                         break;
                     case R.id.profile:
-                        intent = new Intent(Message.this,Profile.class);
+                        intent = new Intent(Message.this, Profile.class);
                         startActivity(intent);
                         break;
                 }
 
 
-
                 return false;
             }
         });
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        List<Drawable> pictures = new ArrayList<Drawable>();
+        for(int i = 0; i<1;i++){
+            animalNames.add("lol " + i);
+        }
+        Drawable image=(Drawable)getResources().getDrawable(R.drawable.message);
+        pictures.add(image);
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycle);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames,pictures);
+        adapter.setClickListener(this::onItemClick);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
 }
