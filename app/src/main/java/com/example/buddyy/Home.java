3ce53f90ce4b45d6287920575983d1c7672c8f48
thestungyer <47674413+thestungyer@users.noreply.github.com>
2021-06-01 10:16:02 +0300

    bug fixes

--- app/src/main/java/com/example/buddyy/Home.java
+++ app/src/main/java/com/example/buddyy/Home.java
@@ -3,6 +3,7 @@ package com.example.buddyy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ import java.util.List;
 public class Home extends AppCompatActivity {
     RecyclerViewHome adapter;
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
 
@@ -77,19 +79,17 @@ public class Home extends AppCompatActivity {
         RecyclerView v = (RecyclerView) findViewById(R.id.recyclerhome);
         v.setNestedScrollingEnabled(false);
 
-        final List<Posts>[] x = new List[]{null};
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
-        reference.addValueEventListener(new ValueEventListener() {
+        reference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Object newPost = dataSnapshot.getValue();
-                x[0] = (List<Posts>) newPost;
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
             }
         });
 
